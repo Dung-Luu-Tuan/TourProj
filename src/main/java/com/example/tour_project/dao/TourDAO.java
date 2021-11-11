90053b90052f758bf992a73f43c22d322d678da3
Administrator <luutuandung102@gmail.com>
2021-11-11 09:31:14 +0700

    source main

---
+++ src/main/java/com/example/tour_project/dao/TourDAO.java
@@ -0,0 +1,43 @@
+package com.example.tour_project.dao;
+
+import com.example.tour_project.models.Tour;
+import com.example.tour_project.utils.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+public class TourDAO {
+    private static SessionFactory factory;
+    public static List<Tour> listTour() {
+        List<Tour> tours = null;
+        factory = HibernateUtil.getSessionFactory();
+        Session session = factory.openSession();
+        try {
+            tours = session.createQuery("from Tour").list();
+            return tours;
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        session.close();
+        return null;
+    }
+
+    public static Tour getDetail(String matour) {
+        Tour tour = null;
+        factory = HibernateUtil.getSessionFactory();
+        Session session = factory.openSession();
+        session.clear();
+        try {
+            session.beginTransaction();
+            tour = session.find(Tour.class, matour);
+//            ArrayList<TourPrice> tourPrices = new ArrayList<>(tour.getPrices());
+//            tour = new Tour(tour.getMatour(), tour.getLoaihinh(), tourPrices);
+            return tour;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/example/tour_project/main.java
@@ -0,0 +1,22 @@
+package com.example.tour_project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("tour-lists.fxml"));
+        stage.setTitle("detail tours");
+        Scene scene = new Scene(root,1000,700 );
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main (String[] args) {
+        launch(args);
+    }
+}
